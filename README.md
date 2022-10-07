9d7ec08fdd1ebcbfaaeea0ab116d499509efdac7
Carlos Eduardo <101234455+CarlosEduardo00@users.noreply.github.com>
2022-10-07 20:46:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Estudo Parallax e ScrollReveal
+
+Projeto criado para estudo do estilo parallax e ScrollReveal para páginas Web
---
+++ script.js
@@ -0,0 +1,11 @@
+// window.sr = ScrollReveal({ reset: true});
+
+// sr.reveal('.area-1', {duration: 1200});
+// sr.reveal('.area-2', {duration: 1200});
+// sr.reveal('.area-3',{duration: 1200});
+
+// sr.reveal('.area-3', {
+//     rotate: { x: 100, y: 100, z: 0},
+//     duration: 2100});
+
+AOS.init();
